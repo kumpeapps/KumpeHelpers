443535fd9b08fd689f94f12589d121d64a79334a
Justin Kumpe <jakumpe@kumpes.com>
2020-10-10 16:25:21 -0500

    Initial Commit

---
+++ KumpeHelpers/KumpeHelpers.h
@@ -0,0 +1,18 @@
+//
+//  KumpeHelpers.h
+//  KumpeHelpers
+//
+//  Created by Justin Kumpe on 10/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KumpeHelpers.
+FOUNDATION_EXPORT double KumpeHelpersVersionNumber;
+
+//! Project version string for KumpeHelpers.
+FOUNDATION_EXPORT const unsigned char KumpeHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KumpeHelpers/PublicHeader.h>
+
+
