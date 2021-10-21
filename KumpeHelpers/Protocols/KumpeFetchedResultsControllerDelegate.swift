654fd756bf05bfff92a4871eb519109562261725
Justin Kumpe <37677675+justinkumpe@users.noreply.github.com>
2021-10-21 18:56:07 -0500

    Revert "Updated for iOS 15"

--- KumpeHelpers/Protocols/KumpeFetchedResultsControllerDelegate.swift
+++ KumpeHelpers/Protocols/KumpeFetchedResultsControllerDelegate.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 import CoreData
 
-protocol KumpeFetchedResultsControllerDelegate: AnyObject {
+protocol KumpeFetchedResultsControllerDelegate: class {
 
     var tableView: UITableView! { get set }
 
