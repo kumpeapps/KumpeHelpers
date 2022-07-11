4fbcd9a82b3c782d83ee95b35c7425fe405cb13f
Justin Kumpe <jakumpe@kumpes.com>
2022-07-11 09:57:21 -0500

    Cleaned up KumpeHelpers.Share function to allow specifying blocked activities

--- KumpeHelpers/Helper Utilities/Share.swift
+++ KumpeHelpers/Helper Utilities/Share.swift
@@ -9,8 +9,8 @@ import UIKit
 
 public class Share {
 
-    /// Share a URL link- Usage: KumpeHelpers.Share.url("url as string", self)
-    public static func url(_ link: String, _ viewController: UIViewController) {
+    /// Share a URL link- Usage: KumpeHelpers.Share.url("url as string", self, blockActivities)
+    public static func url(_ link: String, _ viewController: UIViewController, blockActivities: [UIActivity.ActivityType] = []) {
         dispatchOnMain {
             let url = URL(string: link)!
             let activity = UIActivityViewController(activityItems: [url], applicationActivities: nil)
@@ -18,25 +18,34 @@ public class Share {
         }
     }
 
-    /// Share Text- Usage: KumpeHelpers.Share.url("text as string", self, blockPrint = false)
-    public static func text(_ text: String, _ viewController: UIViewController, blockPrint: Bool = false) {
+    /// Share Text- Usage: KumpeHelpers.Share.url("text as string", self, blockActivities)
+    public static func text(_ text: String, _ viewController: UIViewController, blockActivities: [UIActivity.ActivityType] = []) {
         dispatchOnMain {
             let activity = UIActivityViewController(activityItems: [text], applicationActivities: nil)
-            if blockPrint {
-                activity.excludedActivityTypes = [UIActivity.ActivityType.print]
+            if blockActivities != [] {
+                activity.excludedActivityTypes = blockActivities
             }
             viewController.present(activity, animated: true)
         }
     }
 
-    /// Share an image- Usage: KumpeHelpers.Share.url(UIImage, self, blockPrint = false)
-    public static func image(_ image: UIImage, _ viewController: UIViewController, blockPrint: Bool = false) {
+    /// Share an image- Usage: KumpeHelpers.Share.url(UIImage, self, blockActivities)
+    public static func image(_ image: UIImage, _ viewController: UIViewController, blockActivities: [UIActivity.ActivityType] = []) {
         dispatchOnMain {
             let activity = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-            if blockPrint {
-                activity.excludedActivityTypes = [UIActivity.ActivityType.print]
+            if blockActivities != [] {
+                activity.excludedActivityTypes = blockActivities
             }
             viewController.present(activity, animated: true)
         }
     }
+
+    /// Share Text- Usage: KumpeHelpers.Share.url("text as string", self, blockPrint = false)
+    @available(iOS, obsoleted: 14, renamed: "text(_:_:blockActivities:)")
+    public static func text(_ text: String, _ viewController: UIViewController, blockPrint: Bool = false) {}
+
+    /// Share an image- Usage: KumpeHelpers.Share.url(UIImage, self, blockPrint = false)
+    @available(iOS, obsoleted: 14, renamed: "image(_:_:blockActivities:)")
+    public static func image(_ image: UIImage, _ viewController: UIViewController, blockPrint: Bool = false) {}
+
 }
