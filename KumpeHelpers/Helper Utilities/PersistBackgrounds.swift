71359a4114b031c8d5bdb966b0e8dff0ad798a3f
Justin Kumpe <jakumpe@kumpes.com>
2022-08-14 17:13:39 -0500

    added custom image and iCloud to persistent images

--- KumpeHelpers/Helper Utilities/PersistBackgrounds.swift
+++ KumpeHelpers/Helper Utilities/PersistBackgrounds.swift
@@ -12,50 +12,82 @@ import UIKit
 public class PersistBackgrounds {
 
 // MARK: saveImage
-    public class func saveImage(_ image: UIImage, isBackground: Bool) {
+    public class func saveImage(_ image: UIImage, isBackground: Bool, isCustom: Bool = false, iCloud: Bool = false, iCloudContainer: String? = nil) {
 
         var imageName = "background.png"
 
         if !isBackground {
             imageName = "logo.png"
+        }
+
+        if isCustom {
+            imageName = "custom_\(imageName)"
         }
 
         // Convert to Data
-        if let data = image.pngData() {
-            // Create URL
-            let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-            let url = documents.appendingPathComponent(imageName)
+        guard let data = image.pngData() else {
+            return
+        }
 
-            do {
-                // Write to Disk
-                try data.write(to: url)
-                Logger.log(.action, "\(imageName) saved to \(url)")
+        // Create URL
+        var documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
 
-            } catch {
-            print("Unable to Write Data to Disk (\(error))")
+        if iCloud {
+            DispatchQueue.global().async {
+                documents = FileManager.default.url(forUbiquityContainerIdentifier: iCloudContainer)!.appendingPathComponent("Documents")
             }
         }
+
+        let url = documents.appendingPathComponent(imageName)
+
+        do {
+            // Write to Disk
+            try data.write(to: url)
+            Logger.log(.action, "\(imageName) saved to \(url)")
+
+        } catch {
+            Logger.log(.error, "Unable to Write Data to Disk (\(error))")
+        }
+
     }
 
 // MARK: loadImage
-    public class func loadImage(isBackground: Bool) -> UIImage? {
-
+    public class func loadImage(isBackground: Bool, iCloud: Bool = false, iCloudContainer: String? = nil) -> UIImage? {
+        
         var imageName = "background.png"
+        var documentsUrl: URL = URL(string: "")!
 
         if !isBackground {
             imageName = "logo.png"
         }
 
-      let documentDirectory = FileManager.SearchPathDirectory.documentDirectory
+        let customImageName = "custom_\(imageName)"
+
+        let documentDirectory = FileManager.SearchPathDirectory.documentDirectory
 
         let userDomainMask = FileManager.SearchPathDomainMask.userDomainMask
         let paths = NSSearchPathForDirectoriesInDomains(documentDirectory, userDomainMask, true)
 
-        if let dirPath = paths.first {
-            let imageUrl = URL(fileURLWithPath: dirPath).appendingPathComponent(imageName)
+        if iCloud {
+            DispatchQueue.global().async {
+                documentsUrl = FileManager.default.url(forUbiquityContainerIdentifier: iCloudContainer)!.appendingPathComponent("Documents")
+            }
+        } else if let dirPath = paths.first {
+            documentsUrl = URL(fileURLWithPath: dirPath)
+            let imageUrl = documentsUrl.appendingPathComponent(imageName)
             let image = UIImage(contentsOfFile: imageUrl.path)
             return image
+        }
 
+        let imageUrl = documentsUrl.appendingPathComponent(imageName)
+        let image = UIImage(contentsOfFile: imageUrl.path)
+        let customImageUrl = documentsUrl.appendingPathComponent(customImageName)
+        let customImage = UIImage(contentsOfFile: customImageUrl.path)
+    
+        if customImage != nil {
+            return customImage
+        } else if image != nil {
+            return image
         }
 
         return nil
